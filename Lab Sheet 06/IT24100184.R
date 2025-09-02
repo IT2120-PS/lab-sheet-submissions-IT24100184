007d9e3b94950831f85d94bfe33b48c927308bdb
IT24100184 <IT24100184@my.sliit.lk>
2025-09-02 15:33:40 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24100184.R
@@ -0,0 +1,18 @@
+getwd()
+
+setwd("C:\\Users\\IT24100184\\Desktop\\IT24100184")
+
+getwd()
+
+n <- 50     
+p <- 0.85  
+
+
+1 - pbinom(46, 50, 0.85)
+
+dpois(15, 12)  # lambda = 12
+
+dpois(15, 12)
+
+
+ppois(15, 12, lower.tail = FALSE)
